39d183de14dfc260db08037a8226b5c6612de45a
Eduard Sergeev <eduard.sergeev@gmail.com>
2021-10-18 12:32:06 +1100

    Refactor dotnet test
    Use `Assert` via `using static NUnit.Framework.Assert`

--- test/dotnet/example/Tests.cs
+++ test/dotnet/example/Tests.cs
@@ -1,4 +1,5 @@
 using NUnit.Framework;
+using static NUnit.Framework.Assert;
 using static System.Diagnostics.FileVersionInfo;
 using static System.Reflection.Assembly;
 
@@ -6,27 +7,27 @@ namespace Tests
 {
     public class Version
     {
-        string Expected =>
-            TestContext.Parameters["version"] ?? "0.0.0.0";
+        string Expected => TestContext.Parameters["version"] ?? "0.0.0.0";
+
         System.Reflection.Assembly Assembly =>
             GetExecutingAssembly();
 
         [Test]
         public void AssemblyVersion()
         {
-            Assert.AreEqual(Expected, Assembly.GetName().Version.ToString());
+            AreEqual(Expected, Assembly.GetName().Version.ToString());
         }
 
         [Test]
         public void FileVersion()
         {
-            Assert.AreEqual(Expected, GetVersionInfo(Assembly.Location).FileVersion);
+            AreEqual(Expected, GetVersionInfo(Assembly.Location).FileVersion);
         }
 
         [Test]
         public void ProductVersion()
         {
-            Assert.AreEqual(Expected, GetVersionInfo(Assembly.Location).ProductVersion);
+            AreEqual(Expected, GetVersionInfo(Assembly.Location).ProductVersion);
         }
     }
 }
